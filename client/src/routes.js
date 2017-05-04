85207072262a763b2b9ffcacfefe649a9cc48752
Arjun Nayak <arjunglobal09@gmail.com>
2017-05-04 14:31:16 -0700

    restructure: adding client directory

---
+++ client/src/routes.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+// Import miscellaneous routes and other requirements
+import App from './components/app';
+import NotFoundPage from './components/pages/not-found-page';
+
+// Import static pages
+import HomePage from './components/pages/home-page';
+import ContactPage from './components/pages/contact-page';
+import ComponentSamplesPage from './components/pages/component-samples';
+
+// Import authentication related pages
+import Register from './components/auth/register';
+import Login from './components/auth/login';
+import Logout from './components/auth/logout';
+import ForgotPassword from './components/auth/forgot_password';
+import ResetPassword from './components/auth/reset_password';
+
+// Import dashboard pages
+import Dashboard from './components/dashboard/dashboard';
+import ViewProfile from './components/dashboard/profile/view-profile';
+
+// Import higher order components
+import RequireAuth from './components/auth/require_auth';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="contact-us" component={ContactPage} />
+    <Route path="component-samples" component={RequireAuth(ComponentSamplesPage)} />
+    <Route path="register" component={Register} />
+    <Route path="login" component={Login} />
+    <Route path="logout" component={Logout} />
+    <Route path="forgot-password" component={ForgotPassword} />
+    <Route path="reset-password/:resetToken" component={ResetPassword} />
+
+    <Route path="profile" component={RequireAuth(ViewProfile)} />
+
+    <Route path="dashboard">
+      <IndexRoute component={RequireAuth(Dashboard)} />
+    </Route>
+
+    <Route path="*" component={NotFoundPage} />
+  </Route>
+);
