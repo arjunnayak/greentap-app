0a80b6542e8085d00c1362f4b093dd75540663c4
Derek Nguyen <Derek@dereks-air.attlocal.net>
2017-07-15 10:17:59 -0700

    adding grid system for main content

--- client/src/components/pages/home_page.js
+++ client/src/components/pages/home_page.js
@@ -45,37 +45,49 @@ class HomePage extends Component {
     return (
       <div>
         <Jumbotron>
-          <div className='Jumbotron-content'>
-            <h1>Wholesale Cannabis</h1>
-            <h1>Marketplace</h1>
-            <p>Easily source and discover products from anywhere. Start your new business today.</p>
+          <div className='container'>
+            <div className='Jumbotron-content'>
+              <h1>Wholesale Cannabis</h1>
+              <h1>Marketplace</h1>
+              <p>Easily source and discover products from anywhere. Start your new business today.</p>
 
-          <Navbar.Form>
-              <FormGroup>
-                <FormControl type="text" placeholder="Get notified, enter e-mail" />
-                {' '}
-                <ControlLabel>You are a: </ControlLabel>
-                <FormControl componentClass='select' placeholder='select'>
-                  <option value='Vendor'>Vendor</option>
-                  <option value='Retailer'>Retailer</option>
-                </FormControl>
+              <Navbar.Form>
+                  <FormGroup>
+                    <FormControl type="text" placeholder="Get notified, enter e-mail" />
+                    {' '}
+                    <ControlLabel>You are a: </ControlLabel>
+                    <FormControl componentClass='select' placeholder='select'>
+                      <option value='Vendor'>Vendor</option>
+                      <option value='Retailer'>Retailer</option>
+                    </FormControl>
 
-                <Button type="submit">Submit</Button>
-              </FormGroup>
-          </Navbar.Form>
-
+                    <Button type="submit">Submit</Button>
+                  </FormGroup>
+              </Navbar.Form>
+            </div>
           </div>
         </Jumbotron>
       </div>
     )
   }
 
+// need to add grid system
+renderContent(){
+  return(
+    <Grid>
+      <Row className='show-grid'>
+        <Col cs={12} md={8}>Hellos</Col>
+      </Row>
+    </Grid>
+  )
+}
   render() {
     return (
       <div className='wrapper'>
         {this.renderNav()}
-        <div className="container">
         {this.renderHero()}
+        <div className="container">
+          {this.renderContent()}
         </div>
       </div>
     );
