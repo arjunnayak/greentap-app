44470a764ca1067a1792122227056ad1a36b04fa
Nayak, Arjun <Arjun.Nayak@ge.com>
2017-09-14 00:15:54 -0700

    fix error now that models is gone

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,13 +1,16 @@
-const User = require('../models/user')
+// const User = require('../models/user')
 const setUserInfo = require('../helpers').setUserInfo
 
 //= =======================================
 // User Routes
 //= =======================================
-exports.viewProfile = function (req, res, next) {
+exports.viewProfile = function(req, res, next) {
   const userId = req.params.userId
 
-  if (req.user._id.toString() !== userId) { return res.status(401).json({ error: 'You are not authorized to view this user profile.' }) }
+  if(req.user._id.toString() !== userId) { 
+    return res.status(401).json({ error: 'You are not authorized to view this user profile.' }) 
+  }
+  
   User.findById(userId, (err, user) => {
     if (err) {
       res.status(400).json({ error: 'No user could be found for this ID.' })
@@ -15,7 +18,6 @@ exports.viewProfile = function (req, res, next) {
     }
 
     const userToReturn = setUserInfo(user)
-
     return res.status(200).json({ user: userToReturn })
   })
 }
