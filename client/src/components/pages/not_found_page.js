27009ca70a2df5c94448693bab2615bc5c018afd
Arjun Nayak <arjunglobal09@gmail.com>
2017-11-08 22:21:57 -0800

    update page not found

--- client/src/components/pages/not_found_page.js
+++ client/src/components/pages/not_found_page.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Header from '../template/header';
 
 class NotFoundPage extends Component {
 
   render() {
     return (
-      <div>
-        <h1>404 - Page Not Found</h1>
-        <p>I'm sorry, the page you were looking for cannot be found!</p>
+      <div className="container">
+        <Header />
+        <h1>Page Not Found</h1>
+        <p>I'm sorry, the page you were looking for cannot be found.</p>
       </div>
     );
   }
