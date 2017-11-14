a4728fbcf652fab06224df4064196a7034f15a37
Arjun Nayak <arjunglobal09@gmail.com>
2017-11-13 21:11:03 -0800

    switch header to use login

--- client/src/components/template/header.js
+++ client/src/components/template/header.js
@@ -23,7 +23,7 @@ class Header extends Component {
           <Link to="/dashboard">Dashboard</Link>
         </li>,
         <li key={2}>
-          <Link to="/login">Sign In</Link>
+          <Link to="/login">Login</Link>
         </li>,
         <li key={3}>
           <Link to="/register">Register</Link>
