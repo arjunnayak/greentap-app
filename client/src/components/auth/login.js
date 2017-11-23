2e50a96204c48600231dff7822ea586715e86feb
Arjun Nayak <arjunglobal09@gmail.com>
2017-11-22 21:56:00 -0800

    implement auth forms with semantic ui

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
-import { Link } from 'react-router-dom';
-import { loginUser } from '../../actions/auth';
-import Header from '../template/header';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
+import { loginUser } from '../../actions/auth'
+import Header from '../template/header'
+import { Button, Form, Grid, Message, Segment } from 'semantic-ui-react'
 
 const form = reduxForm({
   form: 'login',
-});
+})
 
 const renderField = field => (
-  <div>
-    <input className="form-control" {...field.input} type={field.type} placeholder={field.placeholder} />
-    {field.touched && field.error && <div className="error">{field.error}</div>}
-  </div>
-);
+  <Form.Input fluid type={field.type} placeholder={field.placeholder} {...field.input} />
+)
 
 class Login extends Component {
   handleFormSubmit(formProps) {
     this.props.loginUser(formProps)
       .then(() => {
-      if(this.props.authenticated) {
-        this.props.history.push("/dashboard");
-      }
-    });
+        if (this.props.authenticated) {
+          this.props.history.push("/dashboard")
+        }
+      })
   }
 
   renderAlert() {
@@ -32,41 +30,35 @@ class Login extends Component {
         <div className="auth-form-error">
           <span><strong>Error!</strong> {this.props.errorMessage}</span>
         </div>
-      );
+      )
     }
   }
 
   render() {
-    const { handleSubmit } = this.props;
-    return (  
+    const { handleSubmit } = this.props
+    return (
       <div>
         <Header />
-        <div className="container cont-space center">
-          <h1 className="form-space">Login</h1>
-          <div className="panel panel-default">
-            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <div className='auth-form'>
+          <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <h1>Login</h1>
               {this.renderAlert()}
-              <div className="panel-body">
-                <Field name="email" component={renderField} type="text" placeholder="Email" />
-              </div>
-              <div className="panel-body">
-                <Field name="password" component={renderField} type="password" placeholder="Password" />
-              </div>
-              <div className="panel-body">
-                <button type="submit" className="btn btn-primary">Login</button>
-              </div>
-              <div className="panel-body">
-                <Link to="/forgot-password">Forgot Password?</Link>
-              </div>
-              <div className="last-link">
-                <Link to="/register">Create Account</Link>
-              </div>
-            </form>
-          </div>
+              <Form size='large' onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <Segment>
+                  <Field name="email" component={renderField} type="text" placeholder="Email" />
+                  <Field name="password" component={renderField} type="password" placeholder="Password" />
+                  <Button primary fluid size='large'>Login</Button>
+                </Segment>
+              </Form>
+              <Message>
+                New to us?{' '}<Link to="/register">Sign Up</Link>
+              </Message>
+            </Grid.Column>
+          </Grid>
         </div>
-    </div>
-
-    );
+      </div>
+    )
   }
 }
 
@@ -75,7 +67,7 @@ function mapStateToProps(state) {
     errorMessage: state.auth.error,
     message: state.auth.message,
     authenticated: state.auth.authenticated,
-  };
+  }
 }
 
-export default connect(mapStateToProps, { loginUser })(form(Login));
+export default connect(mapStateToProps, { loginUser })(form(Login))
--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Field, reduxForm, formValueSelector } from 'redux-form';
-import { registerUser } from '../../actions/auth';
-import Header from '../template/header';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Field, reduxForm, formValueSelector } from 'redux-form'
+import { registerUser } from '../../actions/auth'
+import Header from '../template/header'
+import { Button, Form, Grid, Message, Segment } from 'semantic-ui-react'
 
-const formName = 'register';
+const stateOptions = [
+  { text: 'Alaska', value: 'Alaska' },
+  { text: 'California', value: 'California' },
+  { text: 'Maine', value: 'Maine' },
+  { text: 'Massachusetts', value: 'Massachusetts' },
+  { text: 'Nevada', value: 'Nevada' },
+  { text: 'Oregon', value: 'Oregon' },
+  { text: 'Washington', value: 'Washington' }
+]
 
+const formName = 'register'
 const form = reduxForm({
-  form: formName
-});
+  form: formName,
+  //initial value needed so that the 'state' Form.Select will be passed into formProps on submit
+  initialValues: {
+    state: stateOptions[0].value
+  }
+})
 
-const renderField = field => (
-  <div className="panel-body">
-    <input className="form-control" {...field.input} type={field.type} placeholder={field.placeholder} />
-    {field.touched && field.error && <div className="error">{field.error}</div>}
-  </div>
-);
+const renderField = field => {
+  if(field.type === "text" || field.type === "password") {
+    return ( <Form.Input fluid type={field.type} placeholder={field.placeholder} {...field.input} /> )
+  } else if(field.type === "select") {
+    return ( <Form.Select options={field.options} placeholder={field.placeholder} {...field.input}/>)
+  } else {
+    return null
+  }
+}
 
 class Register extends Component {
   handleFormSubmit(formProps) {
@@ -23,12 +40,12 @@ class Register extends Component {
       .then(() => {
         if (this.props.authenticated) {
           if (this.props.user.business) {
-            this.props.history.push("/dashboard");
+            this.props.history.push("/dashboard")
           } else {
-            this.props.history.push("/");
+            this.props.history.push("/")
           }
         }
-      });
+      })
   }
 
   renderAlert() {
@@ -37,68 +54,65 @@ class Register extends Component {
         <div className="auth-form-error">
           <span>Error: {this.props.errorMessage}</span>
         </div>
-      );
+      )
     }
   }
 
   render() {
-    const { handleSubmit, businessTypeValue } = this.props;
+    const { handleSubmit, businessTypeValue } = this.props
 
     return (
       <div>
         <Header />
-        <div className="container cont-space center">
-          <h1 className="form-space">Register</h1>
-          <div className="panel panel-default">
-            <form className="" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <div className="auth-form">
+          <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <h1 className="form-space">Register</h1>
               {this.renderAlert()}
-              <Field name="firstName" component={renderField} type="text" placeholder="First Name" />
-              <Field name="lastName" component={renderField} type="text" placeholder="Last Name" />
-              <Field name="email" component={renderField} type="text" placeholder="Email" />
-              <Field name="password" component={renderField} type="password" placeholder="Password" />
-              <Field name="confirmPassword" component={renderField} type="password" placeholder="Confirm Password" />
-              <label>Are you a retailer or a brand?</label>
-              <div>
-                <label><Field name="businessType" component="input" type="radio" value="retailer" /> Retailer</label>
-                <label><Field name="businessType" component="input" type="radio" value="brand" /> Brand</label>
-              </div>
-              {
-                businessTypeValue === "brand" ? (
+              <Form size='large' onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <Segment>
+                  <Field name="firstName" component={renderField} type="text" placeholder="First Name" />
+                  <Field name="lastName" component={renderField} type="text" placeholder="Last Name" />
+                  <Field name="email" component={renderField} type="text" placeholder="Email" />
+                  <Field name="password" component={renderField} type="password" placeholder="Password" />
+                  <Field name="confirmPassword" component={renderField} type="password" placeholder="Confirm Password" />
+                  <label>Are you a retailer or a brand?</label>
                   <div>
-                    <Field name="businessName" component={renderField} type="input" placeholder="Business Name" />
-                    <Field name="phone" component={renderField} type="input" placeholder="Phone" />
-                    <Field name="address" component={renderField} type="input" placeholder="Address" />
-                    <Field name="city" component={renderField} type="input" placeholder="City" />
-                    State: <Field name="state" component="select">
-                      <option value="California">California</option>
-                      <option value="Oregon">Oregon</option>
-                      <option value="Washington">Washington</option>
-                    </Field>
-                    <Field name="zip" component={renderField} type="input" placeholder="Zip Code" />
+                    <label><Field name="businessType" component="input" type="radio" value="retailer" /> Retailer</label>
+                    <label><Field name="businessType" component="input" type="radio" value="brand" /> Brand</label>
                   </div>
-                ) : null
-              }
-              <div className="panel-body">
-                <button type="submit" className="btn btn-primary">Create Account</button>
-              </div>
-            </form>
-          </div>
+                  {
+                    businessTypeValue === "brand" ? (
+                      <div>
+                        <Field name="businessName" component={renderField} type="text" placeholder="Business Name" />
+                        <Field name="phone" component={renderField} type="text" placeholder="Phone" />
+                        <Field name="address" component={renderField} type="text" placeholder="Address" />
+                        <Field name="city" component={renderField} type="text" placeholder="City" />
+                        State: <Field name="state" component={renderField} type="select" options={stateOptions} placeholder="State"/>
+                        <Field name="zip" component={renderField} type="text" placeholder="Zip Code" />
+                      </div>
+                    ) : null
+                  }
+                  <Button primary>Create Account</Button>
+                </Segment>
+              </Form>
+            </Grid.Column>
+          </Grid>
         </div>
       </div>
-    );
+    )
   }
 }
 
-const selector = formValueSelector(formName);
+const selector = formValueSelector(formName)
 
 function mapStateToProps(state) {
   return {
     errorMessage: state.auth.error,
-    message: state.auth.message,
     authenticated: state.auth.authenticated,
     user: state.auth.user,
     businessTypeValue: selector(state, 'businessType')
-  };
+  }
 }
 
-export default connect(mapStateToProps, { registerUser })(form(Register));
+export default connect(mapStateToProps, { registerUser })(form(Register))
