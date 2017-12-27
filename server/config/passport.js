512742ce4849bcc0e63c3893eb5fb4af620fcf46
Arjun Nayak <arjunglobal09@gmail.com>
2017-12-27 15:18:37 -0800

    improve performance by serializing/deserializing the entire user rather than making more db calls

--- server/config/passport.js
+++ server/config/passport.js
@@ -40,37 +40,39 @@ module.exports = passport => {
   
   passport.serializeUser((user, done) => {
     console.log('serialize user', user)
-    done(null, user.email);
+    done(null, user);
   })
   
   // used to deserialize the user
-  passport.deserializeUser((email, done) => {
-    const DESERIALIZE_USER = 'SELECT id, email, first_name, last_name, business_type FROM public.user WHERE email = $1;'
-    var user = null;
-    db.task(t => {
-      return t.one(DESERIALIZE_USER, [email])
-        .then(authedUser => {
-          user = authedUser   
-          if(user.business_type === "brand") {
-            const GET_BUSINESS = 'SELECT * FROM public.business WHERE user_id = $1';
-            return t.one(GET_BUSINESS, [user.id])
-          } else {
-            return done(null, user)
-          }
-        })
-        .catch(error => {
-          //failed to get user...have business handler return done(null, false) since it can't be done here
-          //..why it can't be done here: returning here will return to the then block which doesn't indicate an error
-          return
-        })
-    }).then(business => {
-        if(!user) return done(null, false)
-        user.business = business
-        return done(null, user)
-      })
-      .catch(error => {
-        console.log('passport deserialize user error', error)
-        return done(error)
-      })
+  passport.deserializeUser((user, done) => {
+    return done(null, user)
+  //   const DESERIALIZE_USER = 'SELECT id, email, first_name, last_name, business_type FROM public.user WHERE email = $1;'
+  //   var user = null;
+  //   db.task(t => {
+  //     return t.one(DESERIALIZE_USER, [email])
+  //     .then(authedUser => {
+  //       user = authedUser   
+  //       if(user.business_type === "brand") {
+  //         const GET_BUSINESS = 'SELECT * FROM public.business WHERE user_id = $1';
+  //         return t.one(GET_BUSINESS, [user.id])
+  //       } else {
+  //         return done(null, user)
+  //       }
+  //     })
+  //     .catch(error => {
+  //       //failed to get user...have business handler return done(null, false) since it can't be done here
+  //       //..why it can't be done here: returning here will return to the then block which doesn't indicate an error
+  //       return
+  //     })
+  //   }).then(business => {
+  //       if(!user) return done(null, false)
+  //       user.business = business
+  //       console.log('deserialize user', user)
+  //       return done(null, user)
+  //     })
+  //     .catch(error => {
+  //       console.log('passport deserialize user error', error)
+  //       return done(error)
+  //     })
   })
 }
