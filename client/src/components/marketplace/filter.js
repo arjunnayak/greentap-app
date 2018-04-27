061531ceb3ceb8fdef6a292ee4281df38384a827
Arjun Nayak <arjunglobal09@gmail.com>
2018-04-27 02:04:53 -0700

    make filters hideable, all by default are expanded

--- client/src/components/marketplace/filter.js
+++ client/src/components/marketplace/filter.js
@@ -7,19 +7,34 @@ class Filter extends Component {
 
   constructor(props) {
     super(props)
+    this.state = { hiddenOptions: [] }
     this.handleFilterChange = this.handleFilterChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e, data) {
+    let filterType = data.content
+    let hiddenOptions = this.state.hiddenOptions
+    if(hiddenOptions.includes(filterType)) {
+      hiddenOptions = hiddenOptions.filter(i => i !== filterType)
+    } else {
+      hiddenOptions.push(filterType)
+    }
+    this.setState({ hiddenOptions })
   }
 
   render() {
     const options = this.props.options
+    const hiddenOptions = this.state.hiddenOptions
     const filterItems = options.map((item, index) => {
       if(item === null) {
         return null
       }
+      const isHidden = hiddenOptions.includes(item.title)
       return (
         <Menu.Item key={index}>
-          <Accordion.Title content={item.title} index={index} />
-          <Accordion.Content active>
+          <Accordion.Title active={!isHidden} content={item.title} index={index} onClick={this.handleClick}/>
+          <Accordion.Content active={!isHidden}>
             {item.content}
           </Accordion.Content>
         </Menu.Item>
