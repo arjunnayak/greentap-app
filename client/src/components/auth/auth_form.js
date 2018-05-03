e7bccc76f8c10bd15fa4bfc2092c5174f3157903
Arjun Nayak <arjunglobal09@gmail.com>
2018-05-02 19:24:30 -0700

    cleanup

--- client/src/components/auth/auth_form.js
+++ client/src/components/auth/auth_form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import Button from 'semantic-ui-react/dist/commonjs/elements/Button'
 import Menu from 'semantic-ui-react/dist/commonjs/collections/Menu'
@@ -7,32 +7,30 @@ import Segment from 'semantic-ui-react/dist/commonjs/elements/Segment'
 import Container from 'semantic-ui-react/dist/commonjs/elements/Container'
 import '../../styles/auth.css'
 
-class AuthForm extends Component {
-  render() {
-    return (
-      <div id="auth">
-        <Segment textAlign='center' vertical>
-          <Container>
-            <Menu inverted secondary size='large'>
-              <Menu.Item className='greentap'>GreenTap</Menu.Item>
-              <Menu.Item id="loginButton" position='right'>
-                <Button as={Link} to='/login' inverted >Login</Button>
-              </Menu.Item>
-            </Menu>
-          </Container>
-          <Container text>
-            <div className='auth-form'>
-              <Grid textAlign='center' verticalAlign='middle'>
-                <Grid.Column>
-                  {this.props.children}
-                </Grid.Column>
-              </Grid>
-            </div>
-          </Container>
-        </Segment>
-      </div>
-    )
-  }
+const AuthForm = (props) => {
+  return (
+    <div id="auth">
+      <Segment textAlign='center' vertical>
+        <Container>
+          <Menu inverted secondary size='large'>
+            <Menu.Item className='greentap'>GreenTap</Menu.Item>
+            <Menu.Item id="loginButton" position='right'>
+              <Button as={Link} to='/login' inverted >Login</Button>
+            </Menu.Item>
+          </Menu>
+        </Container>
+        <Container text>
+          <div className='auth-form'>
+            <Grid textAlign='center' verticalAlign='middle'>
+              <Grid.Column>
+                { props.children }
+              </Grid.Column>
+            </Grid>
+          </div>
+        </Container>
+      </Segment>
+    </div>
+  )
 }
 
 export default AuthForm
