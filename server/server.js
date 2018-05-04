963bc218ffc2427d53a321aa4f802defdc56a30c
Arjun Nayak <arjunglobal09@gmail.com>
2018-05-03 23:05:33 -0700

    allow requests from s3 in production

--- server/server.js
+++ server/server.js
@@ -36,7 +36,8 @@ app.use(compression())
 
 // Enable CORS from client-side
 app.use((req, res, next) => {
-  if(nodeEnv === 'development' || req.headers.origin === config.client_base_url) {
+  if(nodeEnv === 'development' || req.headers.origin === config.client_base_url 
+    || req.headers.origin === 'http://greentap-client.s3-website.us-west-1.amazonaws.com') {
     res.header('Access-Control-Allow-Origin', req.headers.origin)
   }
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
